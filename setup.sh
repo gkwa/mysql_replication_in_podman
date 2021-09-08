999b7be9f89668f947815b13c051d3178a61db6a
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-08 11:47:27 -0700

    real men dont make git commit comments

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 curl -sSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
 
-apt-get -qy update </dev/null
+sudo apt-get -y update </dev/null
 
-apt-get -y install podman </dev/null
+sudo apt-get -y install podman </dev/null
 
 podman --version
