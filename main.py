f851690e262fbe353681a7517e166c4984185876
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-09 16:05:36 -0700

    dns seems to work now

--- main.py
+++ main.py
@@ -51,13 +51,15 @@ rm -rf ./reptest/
 mkdir -p reptest/{{ pod.containers[0].name }}
 cat <<'__eot__' >reptest/{{ pod.containers[0].name }}/my.cnf
 [mysqld]
-# bind-address = 127.0.0.1
+bind-address = {{ pod.name }}.dns.podman
 __eot__
 {% endfor %}
 
 {% for pod in manifest['pods'] %}
+# bridge mode networking
 podman pod create --name {{ pod.name }} -p {{ manifest['global']['internal_port'] }}{{loop.index}}:{{ manifest['global']['internal_port'] }} --network {{ manifest['global']['network'] }}
-podman container create --log-driver journald --pod={{ pod.name }} -v $PWD/reptest/{{ pod.containers[0].name }}/my.cnf:/etc/my.cnf.d/100-reptest.cnf -v {{ pod.volume }}:/var/lib/mysql/data:Z -e MYSQL_ROOT_PASSWORD=demo -e MYSQL_USER=user -e MYSQL_PASSWORD=pass -e MYSQL_DATABASE=db --name {{ pod.containers[0].name }} registry.redhat.io/rhel8/mysql-80
+podman container create --name {{ pod.containers[0].name }} --rm --health-start-period=80s --log-driver journald --pod={{ pod.name }} -v $PWD/reptest/{{ pod.containers[0].name }}/my.cnf:/etc/my.cnf.d/100-reptest.cnf -v {{ pod.volume }}:/var/lib/mysql/data:Z -e MYSQL_ROOT_PASSWORD=demo -e MYSQL_USER=user -e MYSQL_PASSWORD=pass -e MYSQL_DATABASE=db registry.redhat.io/rhel8/mysql-80
+# podman container create --log-driver journald --pod={{ pod.name }} -v {{ pod.volume }}:/var/lib/mysql/data:Z -e MYSQL_ROOT_PASSWORD=demo -e MYSQL_USER=user -e MYSQL_PASSWORD=pass -e MYSQL_DATABASE=db --name {{ pod.containers[0].name }} registry.redhat.io/rhel8/mysql-80
 podman pod ls
 {% endfor %}
 
@@ -74,10 +76,6 @@ podman volume inspect {{ pod.volume }}
 {%- endfor %}
 
 {{ status() }}
-
-{% for pod in manifest['pods'] %}
-podman volume inspect {{ pod.volume }}
-{%- endfor %}
 
 {% for pod in manifest['pods'] %}
 until podman exec -ti {{ pod.containers[0].name }} bash -c 'mysql --host {{ pod.name }} --user=user --password=pass --execute "SHOW DATABASES;"'; do sleep 5; done;
