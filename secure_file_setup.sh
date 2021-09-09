9f4cfc303aa5627c435c6b31eedaf7d0c1a3420c
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-08 18:12:39 -0700

    real men dont make git commit comments

--- secure_file_setup.sh
+++ secure_file_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dotnet --list-sdks
+dotnet --version
 
 cat <<'__eot__' >global.json
 {
@@ -12,10 +13,13 @@ __eot__
 
 dotnet --list-sdks
 dotnet --version
+
 curl -sflL 'https://raw.githubusercontent.com/appveyor/secure-file/master/install.sh' | bash -e -
-dotnet --version
 
 wget https://download.visualstudio.microsoft.com/download/pr/90e2064f-8786-4e12-95cd-8185fc71f1cb/1a3279320411c489f37142ec656ef0b8/dotnet-sdk-3.1.412-linux-x64.tar.gz
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-3.1.412-linux-x64.tar.gz -C $HOME/dotnet
 export DOTNET_ROOT=$HOME/dotnet
 export PATH=$PATH:$HOME/dotnet
+
+dotnet --list-sdks
+dotnet --version
