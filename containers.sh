6d424d318ffbb7f777a6ef31754d7d3f9528bb28
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-08 23:09:41 -0700

    real men dont make git commit comments

--- containers.sh
+++ containers.sh
@@ -7,11 +7,9 @@ echo XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
 
 cp /tmp/auth.json.decrypted /tmp/auth.json.decrypted.tmp
 cat /tmp/auth.json.decrypted.tmp |python -m base64 -d >/tmp/auth.json.decrypted
+mkdir -p $HOME/.docker/
+cp /tmp/auth.json.decrypted $HOME/.docker/config.json
 
 sudo mkdir -p /run/user/$uid/containers
 sudo cp /tmp/auth.json.decrypted /run/user/$uid/containers/auth.json
 sudo chown -R $uid:$gid /run/user/$uid/containers
-
-mkdir -p $HOME/.docker/
-cat /tmp/auth.json.decrypted |python -m base64
-cp /tmp/auth.json.decrypted $HOME/.docker/config.json
