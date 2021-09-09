bcd0498466b87a817db56e03ac0568fb4c2630cd
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-08 20:25:57 -0700

    real men dont make git commit comments

--- creds.sh
+++ creds.sh
@@ -13,7 +13,7 @@ tmp=/tmp/sensitive
 
 mkdir -p $tmp
 
-cat <<__eot__ >$tmp/auth.json
+cat <<__eot__ >$tmp/auth.json.tmp
 {
   "auths": {
     "registry.redhat.io": {
@@ -22,17 +22,19 @@ cat <<__eot__ >$tmp/auth.json
   }
 }
 __eot__
-cat $tmp/auth.json
+cat $tmp/auth.json.tmp |python -m base64 >$tmp/auth.json
+rm -f $tmp/auth.json.tmp
 
 curl -sflL 'https://raw.githubusercontent.com/appveyor/secure-file/master/install.sh' | bash -e -
 secret=$(python3 -c "import uuid; print(uuid.uuid4())")
 echo -n "$secret" >$tmp/secret
 
 salt=$(./appveyor-tools/secure-file -encrypt $tmp/auth.json -secret $secret -out $tmp/auth.json.enc | sed -e 's#Salt: *##')
+rm -f $tmp/auth.json.enc.tmp
 echo -n "$salt" |python -m base64 >$tmp/salt
 
 # now try decrypting it to verify it all worked
-./appveyor-tools/secure-file -decrypt $tmp/auth.json.enc -secret "$secret" -salt "$salt" -out $tmp/auth.json.decrypted
+./appveyor-tools/secure-file -decrypt $tmp/auth.json.enc -secret "$secret" -salt "$salt" -out $tmp/auth.json.decrypted.tmp
 cat $tmp/auth.json.decrypted
 
 credentials=$(jq -r .auths.'"registry.redhat.io"'.auth $tmp/auth.json.decrypted | python -m base64 -d)
