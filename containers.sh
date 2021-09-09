d3c9611edfb5afd0fdc0482c4dc6a67820330cad
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-08 22:36:55 -0700

    real men dont make git commit comments

--- containers.sh
+++ containers.sh
@@ -3,6 +3,8 @@
 uid=$(id -u)
 gid=$(id -g)
 
+echo XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR
+
 sudo mkdir -p /run/user/$uid/containers
 sudo cp /tmp/auth.json.decrypted /run/user/$uid/containers/auth.json
 sudo chown -R $uid:$gid /run/user/$uid/containers
