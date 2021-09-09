8a60ac0e25be93027281806cdc6561cfcd135b52
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-09 00:32:35 -0700

    real men dont make git commit comments

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ podman volume inspect {{ pod.volume }}
 {%- endfor %}
 
 {% for pod in manifest['pods'] %}
-until podman exec -ti {{ pod.containers[0].name }} bash -c 'mysql --host {{ pod.name }} --user=user --password=pass --execute "SHOW DATABASES;"'; do sleep 3; done;
+until podman exec -ti {{ pod.containers[0].name }} bash -c 'mysql --host {{ pod.name }} --user=user --password=pass --execute "SHOW DATABASES;"'; do sleep 5; done;
 {%- endfor %}
 
 {% for pod in manifest['pods'] %}{% set ip='ip' ~ loop.index %}
