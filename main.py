3cb6ce72c21c035a7786d7a404619ce81c1d3e34
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-11 08:07:51 -0700

    real men dont make git commit comments

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ with open(in_file, "r") as stream:
 
 tmpl_str = """#!/bin/bash
 {%- set global=manifest['global'] %}
-{%- set replication_source=manifest['replication_source'] %}
+{%- set replication=manifest['replication'] %}
 {%- set pods=manifest['pods'] %}
 
 {% macro status() -%}
@@ -193,15 +193,27 @@ podman exec --tty --interactive {{ pod.containers[0].name }} mysql --user={{ glo
 podman exec --tty --interactive {{ pod.containers[0].name }} mysql --user={{ global.user_root }} --password={{ global.user_root_pass }} --host={{ pod.name }}.dns.podman --execute 'SELECT User, Host from mysql.user ORDER BY user;'
 {%- endfor %}
 
-{% for block in replication_source %}
-source_ip=$(podman inspect {{ block.get_ip }} --format '{%- raw -%} {{ {%- endraw -%}.NetworkSettings.Networks.{{ global.network}}.IPAddress{%- raw -%} }} {%- endraw -%}');
-podman exec --tty --interactive {{ block.name }} mysql --user={{ global.user_root }} --password={{ global.user_root_pass }} --host={{ block.pod }} --execute \
+{% for instance in replication %}
+source_ip=$(podman inspect {{ instance.source.container }} --format '{%- raw -%} {{ {%- endraw -%}.NetworkSettings.Networks.{{ global.network}}.IPAddress{%- raw -%} }} {%- endraw -%}');
+podman exec --tty --interactive {{ instance.container }} mysql --host={{ instance.pod }} --user={{ global.user_root }} --password={{ global.user_root_pass }} --execute \
 "CHANGE MASTER TO MASTER_HOST='"$source_ip"',\
 MASTER_USER='{{ global.user_replication }}',\
 MASTER_PASSWORD='{{ global.user_replication_pass }}',\
 MASTER_LOG_FILE='mysql-bin.000003',\
 MASTER_LOG_POS=2856;"
 {%- endfor %}
+
+: <<'END_COMMENT'
+{%- for instance in replication %}
+podman exec --tty --interactive {{ instance.container }} mysql --host={{ instance.pod }} --user={{ global.user_root }} --password={{ global.user_root_pass }} --execute \
+"CHANGE MASTER TO MASTER_HOST='"{{ instance.source.pod }}.dns.podman"',\
+MASTER_USER='{{ global.user_replication }}',\
+MASTER_PASSWORD='{{ global.user_replication_pass }}',\
+MASTER_LOG_FILE='mysql-bin.000003',\
+MASTER_LOG_POS=2856;"
+{%- endfor %}
+END_COMMENT
+
 """
 
 template = jinja2.Template(tmpl_str)
