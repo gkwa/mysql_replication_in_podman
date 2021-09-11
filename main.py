e734c4381e0ab563d67a0dd6bd239ca471ed5e37
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-11 00:01:12 -0700

    real men dont make git commit comments

--- main.py
+++ main.py
@@ -193,8 +193,7 @@ podman exec --tty --interactive {{ pod.containers[0].name }} mysql --user={{ glo
 {%- endfor %}
 
 {%- for pod in pods %}
-{%- set user = global.user_replication ~ "@" ~ "$replica_ip"  %}
-replica_ip=$(podman inspect {{ pod.replica.container }} --format '{%- raw -%} {{ {%- endraw -%}.NetworkSettings.Networks.{{ global.network}}.IPAddress{%- raw -%} }} {%- endraw -%}'); podman exec --tty --interactive {{ pod.containers[0].name }} mysql --user={{ global.user_root }} --password={{ global.user_root_pass }} --host={{ pod.name }}.dns.podman --execute "CHANGE REPLICATION SOURCE TO SOURCE_HOST='"$replica_ip"', SOURCE_USER='{{ global.user_replication }}@"$replica_ip"', SOURCE_PASSWORD='{{ global.user_replication_pass }}', SOURCE_LOG_FILE='mysql-bin.000001', SOURCE_LOG_POS=899;"
+source_ip=$(podman inspect {{ pod.containers[0].name }} --format '{%- raw -%} {{ {%- endraw -%}.NetworkSettings.Networks.{{ global.network}}.IPAddress{%- raw -%} }} {%- endraw -%}'); podman exec --tty --interactive {{ pod.containers[0].name }} mysql --user={{ global.user_root }} --password={{ global.user_root_pass }} --host={{ pod.replica.fqdn.split('.')[0] }} --execute "CHANGE REPLICATION SOURCE TO SOURCE_HOST='"$source_ip"', SOURCE_USER='{{ global.user_replication }}', SOURCE_PASSWORD='{{ global.user_replication_pass }}', SOURCE_LOG_FILE='mysql-bin.000003', SOURCE_LOG_POS=2856;"
 {%- endfor %}
 """
 
