62e0452a9a98fa519d1966f667195bc17969f78a
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-11 09:48:10 -0700

    real men dont make git commit comments

--- main.py
+++ main.py
@@ -208,13 +208,14 @@ podman exec --tty --interactive {{ pod.containers[0].name }} mysql --user={{ glo
 
 # FIXME: MASTER_LOG_POS=2856 is bad, you should fetch it
 {% for block in replication %}
-source_ip=$(podman inspect {{ block.source.container }} --format '{%- raw -%} {{ {%- endraw -%}.NetworkSettings.Networks.{{ global.network}}.IPAddress{%- raw -%} }} {%- endraw -%}');
+source_ip=$(podman inspect {{ block.source.container }} --format '{%- raw -%} {{ {%- endraw -%}.NetworkSettings.Networks.{{ global.network}}.IPAddress{%- raw -%} }} {%- endraw -%}')
+position=$(podman exec --tty --interactive {{ block.source.container }} mysql --user={{ global.user_root }} --password={{ global.user_root_pass }} --host={{ block.source.pod }} --execute 'SHOW MASTER STATUS\G' </dev/null |sed -e '/Position:/!d' -e 's/Position://' -e 's/ //g')
 podman exec --tty --interactive {{ block.instance.container }} mysql --host={{ block.instance.pod }} --user={{ global.user_root }} --password={{ global.user_root_pass }} \
 --execute "CHANGE MASTER TO MASTER_HOST='"$source_ip"',\
 MASTER_USER='{{ global.user_replication }}',\
 MASTER_PASSWORD='{{ global.user_replication_pass }}',\
 MASTER_LOG_FILE='mysql-bin.000001',\
-MASTER_LOG_POS=2856"
+MASTER_LOG_POS=$position"
 {%- endfor %}
 
 # FIXME: it would be really nice to be able to use dns here
