20ab18e7ba21114b920c9812d791690b8e5b8f5a
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-13 18:28:13 -0700

    real men dont make git commit comments

--- test20210913_1.sh
+++ test20210913_1.sh
@@ -63,11 +63,11 @@ SELECT * FROM dummy;
 __eot__
 podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my1p.dns.podman --execute 'SOURCE /tmp/extra2/20210912_1.sql'
 
-until podman exec --env=MYSQL_PWD=root --tty --interactive my1c mysql --host=my1p --user=root --execute 'SHOW DATABASES' </dev/null; do sleep 5; done;
-until podman exec --env=MYSQL_PWD=root --tty --interactive my2c mysql --host=my2p --user=root --execute 'SHOW DATABASES' </dev/null; do sleep 5; done;
-until podman exec --env=MYSQL_PWD=root --tty --interactive my3c mysql --host=my3p --user=root --execute 'SHOW DATABASES' </dev/null; do sleep 5; done;
-until podman exec --env=MYSQL_PWD=root --tty --interactive my4c mysql --host=my4p --user=root --execute 'SHOW DATABASES' </dev/null; do sleep 5; done;
-until podman exec --env=MYSQL_PWD=root --tty --interactive my5c mysql --host=my5p --user=root --execute 'SHOW DATABASES' </dev/null; do sleep 5; done;
+until podman exec --env=MYSQL_PWD=root my1c mysql --host=my1p --user=root --execute 'SHOW DATABASES'; do sleep 5; done;
+until podman exec --env=MYSQL_PWD=root my2c mysql --host=my2p --user=root --execute 'SHOW DATABASES'; do sleep 5; done;
+until podman exec --env=MYSQL_PWD=root my3c mysql --host=my3p --user=root --execute 'SHOW DATABASES'; do sleep 5; done;
+until podman exec --env=MYSQL_PWD=root my4c mysql --host=my4p --user=root --execute 'SHOW DATABASES'; do sleep 5; done;
+until podman exec --env=MYSQL_PWD=root my5c mysql --host=my5p --user=root --execute 'SHOW DATABASES'; do sleep 5; done;
 
 podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my1p.dns.podman --database=ptest --execute 'SELECT * FROM dummy'
 podman exec --env=MYSQL_PWD=root my2c mysql --user=root --host=my2p.dns.podman --database=ptest --execute 'SELECT * FROM dummy'
@@ -77,11 +77,11 @@ podman exec --env=MYSQL_PWD=root my5c mysql --user=root --host=my5p.dns.podman -
 
 echo xxxxxxxxxxxxxxxxxx
 set +o errexit
-podman run --pod=my1p --env=PTDEBUG=1 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my1p.dns.podman,u=root,p=root,P=3306
-podman run --pod=my1p --env=PTDEBUG=1 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my2p.dns.podman,u=root,p=root,P=3306
-podman run --pod=my1p --env=PTDEBUG=1 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my3p.dns.podman,u=root,p=root,P=3306
-podman run --pod=my1p --env=PTDEBUG=1 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my4p.dns.podman,u=root,p=root,P=3306
-podman run --pod=my1p --env=PTDEBUG=1 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my5p.dns.podman,u=root,p=root,P=3306
+podman run --pod=my1p --env=PTDEBUG=0 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my1p.dns.podman,u=root,p=root,P=3306
+#podman run --pod=my1p --env=PTDEBUG=1 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my2p.dns.podman,u=root,p=root,P=3306
+#podman run --pod=my1p --env=PTDEBUG=1 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my3p.dns.podman,u=root,p=root,P=3306
+#podman run --pod=my1p --env=PTDEBUG=1 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my4p.dns.podman,u=root,p=root,P=3306
+#podman run --pod=my1p --env=PTDEBUG=1 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my5p.dns.podman,u=root,p=root,P=3306
 set -o errexit
 
 echo yyyyyyyyyyyyyyyyyy
@@ -93,24 +93,24 @@ podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my5p.dns.podman -
 
 cat <<'__eot__' >reptest/extra2/20210913_10.sql
 USE ptest;
-INSERT INTO dummy VALUES (11, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+INSERT INTO dummy VALUES (11, 'xxxxx');
 SELECT * FROM dummy;
 __eot__
-#podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my1p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_10.sql'
-podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my2p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_10.sql'
-podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my3p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_10.sql'
+podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my1p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_10.sql'
+#podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my2p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_10.sql'
+#podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my3p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_10.sql'
 #podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my4p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_10.sql'
-podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my5p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_10.sql'
+#podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my5p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_10.sql'
 
 cat <<'__eot__' >reptest/extra2/20210913_20.sql
 USE ptest;
-INSERT INTO dummy VALUES (12, 'bbbbbbbbbbbbbb');
+INSERT INTO dummy VALUES (12, 'yyy');
 SELECT * FROM dummy;
 __eot__
-podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my1p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_20.sql'
+#podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my1p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_20.sql'
 #podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my2p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_20.sql'
 #podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my3p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_20.sql'
-podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my4p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_20.sql'
+#podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my4p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_20.sql'
 #podman exec --env=MYSQL_PWD=root my1c mysql --user=root --host=my5p.dns.podman --execute 'SOURCE /tmp/extra2/20210913_20.sql'
 
 echo starting replication
@@ -132,5 +132,5 @@ until grep --silent 'Slave_IO_Running: Yes' <<<"$(podman exec --env=MYSQL_PWD=ro
 until grep --silent 'Slave_SQL_Running: Yes' <<<"$(podman exec --env=MYSQL_PWD=root my5c mysql --user=root --host=my5p.dns.podman --execute 'SHOW SLAVE STATUS\G')"; do sleep 5; done;
 
 set +o errexit
-podman run --pod=my1p --env=PTDEBUG=1 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my1p.dns.podman,u=root,p=root,P=3306
+podman run --pod=my1p --env=PTDEBUG=0 --env=MYSQL_PWD=root percona-toolkit pt-table-checksum --replicate=percona.checksums h=my1p.dns.podman,u=root,p=root,P=3306
 set -o errexit
