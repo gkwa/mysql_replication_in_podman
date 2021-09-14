23a92c54389c3cc030a23db2a9c398ec39982d92
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-14 14:47:02 -0700

    stuff and things

--- setup_debian.sh
+++ setup_debian.sh
@@ -14,7 +14,7 @@ sudo apt-get -qy install \
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
 sudo apt-get update
-sudo apt-get -y upgrade
+# sudo apt-get -y upgrade
 sudo apt-get -y install podman
 
 git clone https://github.com/sstephenson/bats.git /usr/local/src/bats
