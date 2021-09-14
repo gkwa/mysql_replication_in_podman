d6a61df7bf1d3a4efbd41a023320848b00068dc7
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-14 11:39:17 -0700

    fix podman

--- setup_debian.sh
+++ setup_debian.sh
@@ -2,11 +2,12 @@
 
 . /etc/os-release
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-curl -sSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install podman
 
-sudo apt-get -qy update
 sudo apt-get -qy install \
-     podman \
      unzip \
      jq \
      git \
