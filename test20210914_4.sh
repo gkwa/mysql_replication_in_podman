65d7d0bdb90fa9d25229008e820555f117ff2a4e
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-16 08:43:42 -0700

    stuff

--- test20210914_4.sh
+++ test20210914_4.sh
@@ -18,7 +18,6 @@ bind-address                   = my1p.dns.podman
 log_bin                        = /var/log/mysql/mysql-bin.log
 log_slave_updates              = ON
 server_id                      = 1
-auto_increment_offset          = 1
 auto_increment_increment       = 5
 __eot__
 
@@ -28,7 +27,6 @@ bind-address                   = my2p.dns.podman
 log_bin                        = /var/log/mysql/mysql-bin.log
 log_slave_updates              = ON
 server_id                      = 2
-auto_increment_offset          = 2
 auto_increment_increment       = 5
 __eot__
 
@@ -38,7 +36,6 @@ bind-address                   = my3p.dns.podman
 log_bin                        = /var/log/mysql/mysql-bin.log
 log_slave_updates              = ON
 server_id                      = 3
-auto_increment_offset          = 3
 auto_increment_increment       = 5
 __eot__
 
@@ -48,7 +45,6 @@ bind-address                   = my4p.dns.podman
 log_bin                        = /var/log/mysql/mysql-bin.log
 log_slave_updates              = ON
 server_id                      = 4
-auto_increment_offset          = 4
 auto_increment_increment       = 5
 __eot__
 
@@ -58,7 +54,6 @@ bind-address                   = my5p.dns.podman
 log_bin                        = /var/log/mysql/mysql-bin.log
 log_slave_updates              = ON
 server_id                      = 5
-auto_increment_offset          = 5
 auto_increment_increment       = 5
 __eot__
 
