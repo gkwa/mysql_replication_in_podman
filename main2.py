b9b894b6e52a2872c9322fe8c6826129c015f49c
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-17 11:25:47 -0700

    [skip ci] this should work on a machine somewhere

--- main2.py
+++ main2.py
@@ -142,7 +142,7 @@ podman exec --env=MYSQL_PWD={{ global.user_root_pass }} {{ pods[0].containers[0]
 
 {% for pod in pods %}
 podman exec --env=MYSQL_PWD={{ global.user_root_pass }} {{ pods[0].containers[0].name }} mysql --user={{ global.user_root }} --host={{ pod.name }} --execute 'SHOW VARIABLES LIKE "binlog_format"'
-{%- endfor %}
+{% endfor %}
 """
 path = pathlib.Path("test.sh")
 template = jinja2.Template(tmpl_str)
