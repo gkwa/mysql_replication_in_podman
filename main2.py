8316cef93416ca507375db245de3d02d2c352faf
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-17 07:45:53 -0700

    Revert "Remove datadir and absolute path to log_bin"
    This reverts commit c9e5f012a18f8953c26f057cacc4ad86f5e179e1.

--- main2.py
+++ main2.py
@@ -58,7 +58,8 @@ sync_binlog                    = 1
 server_id                      = {{ loop.index }}
 auto_increment_offset          = {{ loop.index }}
 bind-address                   = {{ pod.name }}.dns.podman
-log_bin                        = /var/log/mysql/mysql-bin.log
+datadir                        = /var/log/mysql
+log_bin                        = mysql-bin.log
 log_slave_updates              = ON
 __eot__
 {% endfor %}
