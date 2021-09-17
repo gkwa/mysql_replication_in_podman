04ae856993efce08f17589f22cbd344a0220a3f3
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-17 00:03:14 -0700

    Remove auto_increment_offset

--- main2.py
+++ main2.py
@@ -56,7 +56,6 @@ cat <<'__eot__' >reptest/{{ pod.containers[0].name }}_my.cnf
 innodb_flush_log_at_trx_commit = 1
 sync_binlog                    = 1
 server_id                      = {{ loop.index }}
-auto_increment_offset          = {{ loop.index }}
 bind-address                   = {{ pod.name }}.dns.podman
 datadir                        = /var/log/mysql
 log_bin                        = mysql-bin.log
