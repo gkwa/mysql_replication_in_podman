427a993fd7ff9a17576e8d53273cdd51d40cf5b4
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-16 23:51:34 -0700

    Remove server_id

--- main2.py
+++ main2.py
@@ -55,7 +55,6 @@ cat <<'__eot__' >reptest/{{ pod.containers[0].name }}_my.cnf
 [mysqld]
 innodb_flush_log_at_trx_commit = 1
 sync_binlog                    = 1
-server_id                      = {{ loop.index }}
 auto_increment_offset          = {{ loop.index }}
 bind-address                   = {{ pod.name }}.dns.podman
 datadir                        = /var/log/mysql
