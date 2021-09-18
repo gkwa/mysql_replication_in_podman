ddfcf69a51e571e93f7ae22f9cf3271acb73b7ac
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-18 16:22:04 -0700

    real men dont make git commit comments

--- setup2.sh
+++ setup2.sh
@@ -68,23 +68,26 @@ loop1() {
 podman pull --quiet docker.io/perconalab/percona-toolkit:latest >/dev/null
 podman pull --quiet registry.redhat.io/rhel8/mysql-80 >/dev/null
 
-# set +o errexit
-# podman container stop --log-level debug --ignore my1c my2c my3c my4c my5c
-# set -o errexit
+set +o errexit
+podman container stop --log-level debug --ignore my1c my2c my3c my4c my5c
 set -o errexit
-podman container exists my1c && podman container stop --log-level debug --ignore my1c
-podman container exists my2c && podman container stop --log-level debug --ignore my2c
-podman container exists my3c && podman container stop --log-level debug --ignore my3c
-podman container exists my4c && podman container stop --log-level debug --ignore my4c
-podman container exists my5c && podman container stop --log-level debug --ignore my5c
-set -o errexit
+podman container stop --log-level debug --ignore my1c
+podman container stop --log-level debug --ignore my2c
+podman container stop --log-level debug --ignore my3c
+podman container stop --log-level debug --ignore my4c
+podman container stop --log-level debug --ignore my5c
 
 podman pod exists my1p && podman pod stop my1p --log-level debug --ignore my1p
 podman pod exists my2p && podman pod stop my2p --log-level debug --ignore my2p
 podman pod exists my3p && podman pod stop my3p --log-level debug --ignore my3p
 podman pod exists my4p && podman pod stop my4p --log-level debug --ignore my4p
 podman pod exists my5p && podman pod stop my5p --log-level debug --ignore my5p
-podman wait --condition=stopped my1c my2c my3c my4c my5c
+
+podman container exists my1c && podman wait --condition=stopped my1c
+podman container exists my2c && podman wait --condition=stopped my2c
+podman container exists my3c && podman wait --condition=stopped my3c
+podman container exists my4c && podman wait --condition=stopped my4c
+podman container exists my5c && podman wait --condition=stopped my5c
 podman pod ls
 
 mkdir -p reptest
