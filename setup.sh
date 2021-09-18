b236b3243fdda2dc358eaf2f522da41d8705e122
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-18 16:09:16 -0700

    real men dont make git commit comments

--- setup.sh
+++ setup.sh
@@ -70,12 +70,13 @@ podman pull --quiet registry.redhat.io/rhel8/mysql-80 >/dev/null
 
 set +o errexit
 podman container stop --log-level debug --ignore my1c my2c my3c my4c my5c
-set -o errexit
+# set -o errexit
 podman container exists my1c && podman container stop --log-level debug --ignore my1c
 podman container exists my2c && podman container stop --log-level debug --ignore my2c
 podman container exists my3c && podman container stop --log-level debug --ignore my3c
 podman container exists my4c && podman container stop --log-level debug --ignore my4c
 podman container exists my5c && podman container stop --log-level debug --ignore my5c
+set -o errexit
 
 podman pod exists my1p && podman pod stop my1p --log-level debug --ignore my1p
 podman pod exists my2p && podman pod stop my2p --log-level debug --ignore my2p
