8b1242bd0b85877191bf39a32335036de00adfa3
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-19 12:56:43 -0700

    real men dont make git commit comments

--- main2.py
+++ main2.py
@@ -56,6 +56,12 @@ expanded = tmpl.render(manifest=manifest, test_name=path.stem)
 path.write_text(expanded)
 path.chmod(path.stat().st_mode | stat.S_IEXEC)
 
+path = pathlib.Path("reset_containers.sh")
+tmpl = env.get_template(f"{path.stem}.j2")
+expanded = tmpl.render(manifest=manifest, test_name=path.stem)
+path.write_text(expanded)
+path.chmod(path.stat().st_mode | stat.S_IEXEC)
+
 cmd = ["shfmt", "-w", "-s", "-i", "4", "*.sh"]
 cmd = ' '.join(cmd)
 
