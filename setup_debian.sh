4c53992d36784d3b9ae223a726a1e9c14a550bff
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-20 06:22:34 -0700

    real men dont make git commit comments

--- setup_debian.sh
+++ setup_debian.sh
@@ -7,16 +7,19 @@ apt-get -qqy install \
     python3-pip \
     python3-venv
 
+# run containers with podman
 . /etc/os-release
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 curl -sSL "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | apt-key add -
 apt-get update
 apt-get -qqy install podman
 
+# allow nice testing output
 git clone --quiet --depth 1 https://github.com/sstephenson/bats.git /tmp/bats
 pushd /tmp/bats >/dev/null
 ./install.sh /usr/local
 popd >/dev/null
 
+# allow pretty-printing bash scripts
 curl -sSLo /tmp/shfmt https://github.com/mvdan/sh/releases/download/v2.6.4/shfmt_v2.6.4_linux_386
 install /tmp/shfmt /usr/local/bin/shfmt
