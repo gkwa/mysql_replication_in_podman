4495cd5cf2354f1a395c8cc489e195f22bd5931b
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-28 06:50:21 -0700

    real men dont make git commit comments

--- build_image.sh
+++ build_image.sh
@@ -5,7 +5,7 @@ set -o errexit
 container=$(buildah from python:bullseye)
 buildah config --label maintainer="Taylor Monacelli <taylormonacelli@gmail.com>" $container
 buildah copy $container ./requirements.txt /tmp/requirements.txt
-buildah run $container pip install --requirements /tmp/requirements.txt --upgrade pip
+buildah run $container pip install --requirement /tmp/requirements.txt --upgrade pip
 buildah run $container pip list
 buildah config --workingdir /data $container
 buildah commit --format docker $container dbeval:latest
