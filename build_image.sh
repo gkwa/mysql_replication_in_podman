d55101dcbb9bbab0d0b105092d26677260922cd2
Taylor Monacelli <taylormonacelli@gmail.com>
2021-09-27 22:55:12 -0700

    [skip ci] this should work on a machine somewhere

--- build_image.sh
+++ build_image.sh
@@ -3,27 +3,9 @@
 set -o errexit
 
 container=$(buildah from python:bullseye)
-
-# Labels are part of the "buildah config" command
 buildah config --label maintainer="Taylor Monacelli <taylormonacelli@gmail.com>" $container
-
-cat <<'__eot__' >/tmp/requirements.txt
-monacelli_pylog_prefs
-ipython
-django-extensions
-Django
-humanfriendly
-Jinja2
-Pandas
-PyYAML
-mysqlclient
-pydantic
-wheel
-__eot__
-
-buildah copy $container /tmp/requirements.txt /tmp/requirements.txt
-buildah run $container pip install -r /tmp/requirements.txt
+buildah copy $container ./requirements.txt /tmp/requirements.txt
+buildah run $container pip install --requirements /tmp/requirements.txt --upgrade pip
 buildah run $container pip list
 buildah config --workingdir /data $container
-# buildah config --entrypoint /usr/local/bin/hello $container
 buildah commit --format docker $container dbeval:latest
